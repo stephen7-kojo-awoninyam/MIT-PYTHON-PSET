fd96ce14382f6887cb9220e3ac1eee2077cc74ca
Aoninyam Stephen Kojo <stephenkojoawoninyam@gmail.com>
2023-12-02 12:27:03 +0000

    oop in python

---
+++ oop/free.js
@@ -0,0 +1,28 @@
+/*function change(words){
+    var rest=""
+
+    for(let i=0;i<words.split(" ").length;i++)
+    {
+        if(words[i]==":)"){
+            rest+=words.replaceAll(":)","laugh")
+        }
+        else if(words[i]==":(")
+        {
+            rest+=words.replaceAll(":(","sad")
+     }
+     
+}
+    return rest
+}
+
+console.log(change("i am :)"))*/
+
+// function rage(rest){
+//     for
+// }
+// console.log(rage("we are going"))
+
+let camel= "FirstName"
+var snake= camel.split(/[A-Z]/)
+console.log(snake.join("_"))
+
